51016b9bc4378557a8be698c949fbb64593fdc1f
macandrew6 <mr.betterandrewmac@gmail.com>
2020-08-25 12:22:30 -0700

    assigns ids

--- public/script.js
+++ public/script.js
@@ -3,18 +3,26 @@ const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
 let myVideoStream;
 navigator.mediaDevices
   .getUserMedia({
-    // video: true,
-    // audio: true,
+    video: true,
+    audio: true,
   })
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
   });
 
-socket.emit("join-room", ROOM_ID);
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
 
 socket.on("user-connected", () => {
   connectToNewUser();
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/:room", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId) => {
+  socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user-connected");
   });
